b9d05e2936cb3da32855c4fa329f3372f165259b
Erik Olofsson <erik.olofsson@favro.com>
2026-05-25 09:49:51 +0200

    Update for Meteor 3.4 / Blaze 3 compatibility

--- package.js
+++ package.js
@@ -5,26 +5,24 @@ Package.describe({
   git: 'https://github.com/iron-meteor/iron-core'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.2');
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@3.4');
 
   // dependencies
   api.use('underscore');
   api.use('ejson'); // for cloning
 
-  // remove migrated version
-  api.use('cmather:iron-core@0.2.0', {weak: true});
-
-  api.add_files('lib/version_conflict_error.js');
-  api.add_files('lib/iron_core.js');
+  api.addFiles('lib/version_conflict_error.js');
+  api.addFiles('lib/iron_core.js');
 
   // symbol exports
   api.export('Iron');
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
+  api.versionsFrom('METEOR@3.4');
   api.use('iron:core');
   api.use('tinytest');
   api.use('test-helpers');
-  api.add_files('test/iron_core_test.js');
+  api.addFiles('test/iron_core_test.js');
 });
